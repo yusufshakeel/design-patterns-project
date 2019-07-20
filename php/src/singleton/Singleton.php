fbbf8111a08cc6fbee15a06c549d0c213d41fa20
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2019-07-20 15:03:59 +0530

    update

--- php/src/singleton/Singleton.php
+++ php/src/singleton/Singleton.php
@@ -1,7 +1,28 @@
 <?php
 
-
+/**
+ * Class Singleton
+ */
 class Singleton
 {
+    // this holds the instance of the class
+    private static $instance = null;
 
+    // this will create an instance of a class
+    // note! it is private to prevent instance creation from outside the class
+    private function __construct()
+    {
+        // some code...
+    }
+
+    private static function getInstance()
+    {
+        // if instance of the class doesn't exists then create one
+        if (self::$instance == null) {
+            self::$instance = new Singleton();
+        }
+
+        // return instance of the class
+        return self::$instance;
+    }
 }
