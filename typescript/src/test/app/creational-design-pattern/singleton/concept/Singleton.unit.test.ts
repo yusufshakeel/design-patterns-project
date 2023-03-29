b3c1e6d437d8d688e58076a68247d0bde802c935
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2023-03-29 18:14:45 +0530

    singleton

---
+++ typescript/src/test/app/creational-design-pattern/singleton/concept/Singleton.unit.test.ts
@@ -0,0 +1,10 @@
+import Singleton from '../../../../../app/creational-design-pattern/singleton/concept/Singleton';
+
+describe('Testing Singleton - concept', () => {
+  test('Should be able to create only one instance', () => {
+    const instance1 = Singleton.getInstance();
+    const instance2 = Singleton.getInstance();
+    expect(instance1).toStrictEqual(instance2);
+    expect(instance1.doSomething()).toBe('Did something...');
+  });
+});
